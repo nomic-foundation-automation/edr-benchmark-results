4216da4a2be05dd9bb089507c201693dd1886374
github-action-benchmark <github@users.noreply.github.com>
2024-03-20 08:39:06 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ccdc0bbd2a2901058ec0953e9660161ee8a5e527

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710898619215,
+  "lastUpdate": 1710923946577,
   "repoUrl": "https://github.com/NomicFoundation/hardhat",
   "entries": {
     "Benchmark": [
@@ -63,6 +63,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 43340.359523,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Wodann@users.noreply.github.com",
+            "name": "Wodann",
+            "username": "Wodann"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ccdc0bbd2a2901058ec0953e9660161ee8a5e527",
+          "message": "build: bump revm to 7.1 (#4968)",
+          "timestamp": "2024-03-20T08:31:53+01:00",
+          "tree_id": "4f475fb97749c4516c35420473edebd448dc6cfa",
+          "url": "https://github.com/NomicFoundation/hardhat/commit/ccdc0bbd2a2901058ec0953e9660161ee8a5e527"
+        },
+        "date": 1710923946333,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 3860515.5615020003,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 833098.614253,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_8b12f83a",
+            "value": 87442.812168,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 72999.77622,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 3350.312651,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 18606.350175,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 2801336.558648,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 43681.137387,
             "unit": "ms"
           }
         ]
