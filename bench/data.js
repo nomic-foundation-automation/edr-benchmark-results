230447957fbeb58e2476cfd8852b241da9b05171
github-action-benchmark <github@users.noreply.github.com>
2024-03-26 09:08:06 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3f80e10e9ed09dfc526b89e63e2a5a91b3ead6fb

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710923946577,
+  "lastUpdate": 1711444086261,
   "repoUrl": "https://github.com/NomicFoundation/hardhat",
   "entries": {
     "Benchmark": [
@@ -127,6 +127,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 43681.137387,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victorio.franco@gmail.com",
+            "name": "Franco Victorio",
+            "username": "fvictorio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f80e10e9ed09dfc526b89e63e2a5a91b3ead6fb",
+          "message": "Merge pull request #4991 from NomicFoundation/feat/cancun-mainnet-hardfork\n\nfeat: add Cancun activation block number",
+          "timestamp": "2024-03-26T09:01:13+01:00",
+          "tree_id": "3d8c86f1df3f6b41a00ef75043f1e4b414fa8cf3",
+          "url": "https://github.com/NomicFoundation/hardhat/commit/3f80e10e9ed09dfc526b89e63e2a5a91b3ead6fb"
+        },
+        "date": 1711444085167,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 3860524.832779,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 828637.023925,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_8b12f83a",
+            "value": 87280.830208,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 73792.466586,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 3829.369183,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 17918.866507,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 2805315.273343,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 43751.003027,
             "unit": "ms"
           }
         ]
