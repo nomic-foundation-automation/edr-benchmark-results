91bac34410aab26d9fccbb1e82aabd4536988620
github-action-benchmark <github@users.noreply.github.com>
2024-04-19 17:24:31 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 6447e809b00fa13348c097080deb4042ad7453b1

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713545951456,
+  "lastUpdate": 1713547471825,
   "repoUrl": "https://github.com/NomicFoundation/hardhat",
   "entries": {
     "Benchmark": [
@@ -1446,6 +1446,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 29335.327427,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "5764438+agostbiro@users.noreply.github.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6447e809b00fa13348c097080deb4042ad7453b1",
+          "message": "fix: avoid transferring traces to JS (#5139)",
+          "timestamp": "2024-04-19T19:09:51+02:00",
+          "tree_id": "283a1905ab0bb3cc260cbf1e0bf69823e623fd55",
+          "url": "https://github.com/NomicFoundation/hardhat/commit/6447e809b00fa13348c097080deb4042ad7453b1"
+        },
+        "date": 1713547470639,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 352294.460247,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 34099.884137,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 19815.361928,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 21705.475165,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 1589.348334,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 7619.083018,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 260652.573026,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 6812.734639,
             "unit": "ms"
           }
         ]
