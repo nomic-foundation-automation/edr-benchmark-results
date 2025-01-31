3a39c46e450e95e341a56573290df09a02e0a8e9
github-action-benchmark <github@users.noreply.github.com>
2025-01-31 16:06:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c3b450d26af771830959da95e400f6ed54ebf7bb

--- bench/data.js
+++ bench/data.js
@@ -1,72 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738270072218,
+  "lastUpdate": 1738339578872,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "Wodann@users.noreply.github.com",
-            "name": "Wodann",
-            "username": "Wodann"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8ae31b90584fd61c4073bf335ecb13c4550fa438",
-          "message": "fix: prevent crash when returning large JSON responses (#569)",
-          "timestamp": "2024-08-01T13:27:18Z",
-          "tree_id": "fa980804b28bccc9e5be84fd872107ab4ec3e80f",
-          "url": "https://github.com/NomicFoundation/edr/commit/8ae31b90584fd61c4073bf335ecb13c4550fa438"
-        },
-        "date": 1722521875510,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 349832.628625,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 34372.082341,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 21919.892142,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 22031.037587,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 1680.669709,
-            "unit": "ms"
-          },
-          {
-            "name": "seaport_4f4e7c20",
-            "value": 7027.088325000001,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 255954.598109,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 6847.260412,
-            "unit": "ms"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3197,6 +3133,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 5892.225004,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bas@nomic.foundation",
+            "name": "Bas van Gijzel",
+            "username": "nebasuke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c3b450d26af771830959da95e400f6ed54ebf7bb",
+          "message": "Update checkout and cache actions. (#785)",
+          "timestamp": "2025-01-31T15:44:15Z",
+          "tree_id": "617f5fcc10dbd628caa565f6b8d619e7112d7b34",
+          "url": "https://github.com/NomicFoundation/edr/commit/c3b450d26af771830959da95e400f6ed54ebf7bb"
+        },
+        "date": 1738339578471,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 282040.27601300005,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 27692.108664000003,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 13806.095319,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 16620.360837,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 977.171068,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 4946.691388,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 211809.96412000002,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 6187.884617,
             "unit": "ms"
           }
         ]
