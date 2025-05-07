5575b6478eec4dc7f9b1bc2c2e3181137a03626e
github-action-benchmark <github@users.noreply.github.com>
2025-05-07 15:53:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8396d70f3ff2e785f5ab014fe294cc72c7b2d4dd

--- bench/data.js
+++ bench/data.js
@@ -1,67 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745877555452,
+  "lastUpdate": 1746633222232,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "5764438+agostbiro@users.noreply.github.com",
-            "name": "Agost Biro",
-            "username": "agostbiro"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "ec998cd83832c89ed9324c6b889bde718f8bd6ca",
-          "message": "misc: make crate names explicit (#690)",
-          "timestamp": "2024-10-02T10:27:06Z",
-          "tree_id": "c27b38c6fe622e3c2750f01b668500f26d426b9f",
-          "url": "https://github.com/NomicFoundation/edr/commit/ec998cd83832c89ed9324c6b889bde718f8bd6ca"
-        },
-        "date": 1727866255090,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 320467.94872399996,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 31738.737762,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 18946.963053,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 19167.379633,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 1443.380142,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 236197.29296,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 6275.171424,
-            "unit": "ms"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -2999,6 +2940,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 5695.720169,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Wodann@users.noreply.github.com",
+            "name": "Wodann",
+            "username": "Wodann"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "8396d70f3ff2e785f5ab014fe294cc72c7b2d4dd",
+          "message": "refactor: allow timestamp-based hardfork activations in N-API (#902)",
+          "timestamp": "2025-05-07T15:12:10Z",
+          "tree_id": "275c5a696b492f1384e292806408fbd4d4f5eb8d",
+          "url": "https://github.com/NomicFoundation/edr/commit/8396d70f3ff2e785f5ab014fe294cc72c7b2d4dd"
+        },
+        "date": 1746633221787,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 248392.877101,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 26001.011171,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 13333.892316,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 14994.882932,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 886.5442810000001,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_585b2ef8",
+            "value": 6878.0636540000005,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 181009.546105,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 5288.936642000001,
             "unit": "ms"
           }
         ]
