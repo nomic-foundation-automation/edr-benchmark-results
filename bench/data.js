b31b79bf9b979f8ae91b2fe20aa04a85ba828506
github-action-benchmark <github@users.noreply.github.com>
2025-07-01 10:10:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 3e7551b2b1600ed46ea54bb71f445dbf3a361673

--- bench/data.js
+++ bench/data.js
@@ -1,14 +1,14 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751044551790,
+  "lastUpdate": 1751364602102,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
       {
         "commit": {
           "author": {
-            "email": "41898282+github-actions[bot]@users.noreply.github.com",
-            "name": "github-actions[bot]",
-            "username": "github-actions[bot]"
+            "email": "5764438+agostbiro@users.noreply.github.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
           },
           "committer": {
             "email": "noreply@github.com",
@@ -16,406 +16,57 @@ window.BENCHMARK_DATA = {
             "username": "web-flow"
           },
           "distinct": true,
-          "id": "4959ad3dfd104c0f53ba6dc9e4f786bf1e36b169",
-          "message": "edr-0.10.0 (#827)\n\nCo-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>",
-          "timestamp": "2025-03-10T10:25:08Z",
-          "tree_id": "585eefc3f4b13945a1e292eb91a30ed7ad41911f",
-          "url": "https://github.com/NomicFoundation/edr/commit/4959ad3dfd104c0f53ba6dc9e4f786bf1e36b169"
+          "id": "3e7551b2b1600ed46ea54bb71f445dbf3a361673",
+          "message": "ci: fix hh2 benchmark (#980)",
+          "timestamp": "2025-07-01T09:46:41Z",
+          "tree_id": "adc0fa98958acb51579c6d8d8f2b605c3af6dabf",
+          "url": "https://github.com/NomicFoundation/edr/commit/3e7551b2b1600ed46ea54bb71f445dbf3a361673"
         },
-        "date": 1741603656053,
+        "date": 1751364601608,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
             "name": "All Scenarios",
-            "value": 279115.007876,
+            "value": 284936.79652399995,
             "unit": "ms"
           },
           {
             "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 28366.733233,
+            "value": 29175.188953,
             "unit": "ms"
           },
           {
             "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14008.827823000001,
+            "value": 14628.950898,
             "unit": "ms"
           },
           {
             "name": "rocketpool_6a9dbfd8",
-            "value": 16965.208559,
+            "value": 17012.780456,
             "unit": "ms"
           },
           {
             "name": "safe-contracts_914d0f8",
-            "value": 925.7473380000001,
+            "value": 920.0234169999999,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 4982.335366,
             "unit": "ms"
           },
           {
             "name": "synthetix_9a3a109f",
-            "value": 208110.906551,
+            "value": 211868.954897,
             "unit": "ms"
           },
           {
             "name": "uniswap-v3-core_d8b1c63",
-            "value": 5815.632532,
+            "value": 6348.562537,
             "unit": "ms"
           }
         ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "bas@nomic.foundation",
-            "name": "Bas van Gijzel",
-            "username": "nebasuke"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "5c67a87bc2f4c3cebe0081c90f56271e9e8c95b2",
-          "message": "Add contributor guidelines for small PRs and airdrop farming. (#828)",
-          "timestamp": "2025-03-10T11:21:43Z",
-          "tree_id": "812590fd8c73a4b1d730145e638f96efaa347e22",
-          "url": "https://github.com/NomicFoundation/edr/commit/5c67a87bc2f4c3cebe0081c90f56271e9e8c95b2"
-        },
-        "date": 1741607056069,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 277627.938013,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 28193.666557,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 13838.81963,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 16514.181734,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 908.427235,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 207437.766686,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 5736.553204999999,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "bas@nomic.foundation",
-            "name": "Bas van Gijzel",
-            "username": "nebasuke"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "66c70521fb29dbf6631887b51975ce826d27324a",
-          "message": "Backport for HH2: refactor: generate non-const enum in d.ts (#724) (#832)\n\n* refactor: generate non-const enum in d.ts (#724)\n\n* Regenerate index after fixing merge conflicts.\n\n---------\n\nCo-authored-by: Wodann <Wodann@users.noreply.github.com>",
-          "timestamp": "2025-03-13T15:47:00Z",
-          "tree_id": "480f744da2f044094fec94a66d8908a2032bfcd8",
-          "url": "https://github.com/NomicFoundation/edr/commit/66c70521fb29dbf6631887b51975ce826d27324a"
-        },
-        "date": 1741882242010,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 283565.065862,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 28385.235707,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14435.583941,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 17190.848449999998,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 943.419215,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 211977.102045,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 5711.860965,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "Wodann@users.noreply.github.com",
-            "name": "Wodann",
-            "username": "Wodann"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "60112f13fbedc541729194dc86e2dea5a50b372d",
-          "message": "ci: update op workflow to match feat/multichain (#846)",
-          "timestamp": "2025-03-28T16:21:19Z",
-          "tree_id": "ce39f6d1703e83f7141f32b010b4394c87563871",
-          "url": "https://github.com/NomicFoundation/edr/commit/60112f13fbedc541729194dc86e2dea5a50b372d"
-        },
-        "date": 1743180302655,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 281982.461761,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 28617.485516,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14546.577825,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 16422.801032000003,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 1000.0609,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 210111.916432,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 6099.713833,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "Wodann@users.noreply.github.com",
-            "name": "Wodann",
-            "username": "Wodann"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "5406076ff3380837106ba1ea886d899e03dfaa92",
-          "message": "test: remove failing tests for unsupported receipt types (#863)",
-          "timestamp": "2025-04-07T17:30:32Z",
-          "tree_id": "d10f9d39f80ef4df78223f701750b8cfd8dfd5bd",
-          "url": "https://github.com/NomicFoundation/edr/commit/5406076ff3380837106ba1ea886d899e03dfaa92"
-        },
-        "date": 1744048445185,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 279977.267947,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 27251.611837,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14866.267883,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 17024.369869000002,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 935.8465220000002,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 209388.842782,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 5933.3905030000005,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "fg@frang.io",
-            "name": "Francisco Giordano",
-            "username": "frangio"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "d60b0f2c9e8f4fd022e6159018aec94490172ffa",
-          "message": "docs: update getting started and related code (#862)\n\n* update build instructions\n\n* add note about openssl build deps\n\n* remove indent_size editorconfig for rust\n\n* move test that requires internet connection\n\n* add note about provider tokens\n\n* update hardhat test instructions\n\n* format\n\n* rerun ci in new branch",
-          "timestamp": "2025-04-08T16:02:28Z",
-          "tree_id": "a278a0dc015f089c64f047ed3bbdb9d07b012a20",
-          "url": "https://github.com/NomicFoundation/edr/commit/d60b0f2c9e8f4fd022e6159018aec94490172ffa"
-        },
-        "date": 1744129506127,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 285724.95282500004,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 29426.808667999998,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14160.542447,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 16363.179561,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 934.6191970000001,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 214047.216494,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 5777.82027,
-            "unit": "ms"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "41898282+github-actions[bot]@users.noreply.github.com",
-            "name": "github-actions[bot]",
-            "username": "github-actions[bot]"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8287acac4876a9ca04e7f274c9a08aec50692d5d",
-          "message": "edr-0.11.0 (#834)\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
-          "timestamp": "2025-04-16T18:39:09Z",
-          "tree_id": "eed8ca35b605cd27c2916cd6b765cf30161d84fe",
-          "url": "https://github.com/NomicFoundation/edr/commit/8287acac4876a9ca04e7f274c9a08aec50692d5d"
-        },
-        "date": 1744830159278,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 280934.47001,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 28693.876704,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 14216.482865,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 16627.456532,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 959.0222730000002,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 209361.704867,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 6069.021456,
-            "unit": "ms"
-          }
-        ]
-      },
+      }
     ]
   }
 }
