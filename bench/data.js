891fe9e0458745cf433a1f871e00289e6aac4009
github-action-benchmark <github@users.noreply.github.com>
2025-07-02 12:46:12 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7e12f647065f5c3ae0c33042fe2e762166ed2d0e

--- bench/data.js
+++ bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751364602102,
+  "lastUpdate": 1751460372961,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
@@ -63,6 +63,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 6348.562537,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "5764438+agostbiro@users.noreply.github.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "7e12f647065f5c3ae0c33042fe2e762166ed2d0e",
+          "message": "fix: avoid panic on stack trace for receive with modifier (#957) (#987)",
+          "timestamp": "2025-07-02T12:10:25Z",
+          "tree_id": "d4296ab2e20e808d353f0f7cef3f1c70e7af1c2c",
+          "url": "https://github.com/NomicFoundation/edr/commit/7e12f647065f5c3ae0c33042fe2e762166ed2d0e"
+        },
+        "date": 1751460372483,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 282050.61406099994,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 29039.446938,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 14718.075932,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 16830.217116,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 1034.0804170000001,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_4f4e7c20",
+            "value": 5227.29206,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 209311.696761,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 5889.804837,
             "unit": "ms"
           }
         ]
