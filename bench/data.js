52bf90447e1ece97a3f4430fd5805e2504f58d7e
github-action-benchmark <github@users.noreply.github.com>
2025-07-31 17:59:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 74b1f056a710032a7b8023e0676668a065bc4a35

--- bench/data.js
+++ bench/data.js
@@ -1,72 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753935185170,
+  "lastUpdate": 1753984756421,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "Wodann@users.noreply.github.com",
-            "name": "Wodann",
-            "username": "Wodann"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "ee8732903e85a6d6fea770f008c5604674cf8e5b",
-          "message": "refactor: make Evm generic in Foundry crates (#929)",
-          "timestamp": "2025-06-19T15:41:16Z",
-          "tree_id": "6f1553f3ad0a3df12226924bba7f6a85288c7054",
-          "url": "https://github.com/NomicFoundation/edr/commit/ee8732903e85a6d6fea770f008c5604674cf8e5b"
-        },
-        "date": 1750352901653,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 215520.74240400002,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 20906.229276000002,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 11264.169339,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 12240.649136,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 737.7774820000001,
-            "unit": "ms"
-          },
-          {
-            "name": "seaport_585b2ef8",
-            "value": 5681.938505,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 160171.053369,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 4518.925297000001,
-            "unit": "ms"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3199,6 +3135,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 4456.828783999999,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fg@frang.io",
+            "name": "Francisco Giordano",
+            "username": "frangio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "74b1f056a710032a7b8023e0676668a065bc4a35",
+          "message": "feat: include created contract address in trace (#1031)",
+          "timestamp": "2025-07-31T16:31:54Z",
+          "tree_id": "bcff9ed50b5b07ac9436b601c25bd231d075a836",
+          "url": "https://github.com/NomicFoundation/edr/commit/74b1f056a710032a7b8023e0676668a065bc4a35"
+        },
+        "date": 1753984755975,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 216557.540376,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 21092.996084,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 10752.716133,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 12639.585246,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 764.714382,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_585b2ef8",
+            "value": 5731.394569,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 160965.590068,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 4610.543894,
             "unit": "ms"
           }
         ]
