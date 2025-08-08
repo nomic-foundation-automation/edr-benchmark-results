11117a651f5d1e31cfc4e61e51edbed50864685e
github-action-benchmark <github@users.noreply.github.com>
2025-08-08 20:43:51 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 17be322b56637a263182e4f1f8895539a9f94e3b

--- bench/data.js
+++ bench/data.js
@@ -1,72 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754668205302,
+  "lastUpdate": 1754685831641,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "fg@frang.io",
-            "name": "Francisco Giordano",
-            "username": "frangio"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "3df31b827566857baec5e80f85e3ed0c8b7eb7b9",
-          "message": "chore: disable Codecov checks (#982)",
-          "timestamp": "2025-07-01T13:26:47Z",
-          "tree_id": "eb924e4e85caa250c7b82650a6e94cb8eec93841",
-          "url": "https://github.com/NomicFoundation/edr/commit/3df31b827566857baec5e80f85e3ed0c8b7eb7b9"
-        },
-        "date": 1751377895684,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 213568.058863,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 20539.045757,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 10452.241709,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 12342.466694,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 744.4075599999999,
-            "unit": "ms"
-          },
-          {
-            "name": "seaport_585b2ef8",
-            "value": 5363.0856779999995,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 159685.40268600002,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 4441.408778999999,
-            "unit": "ms"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3199,6 +3135,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 4414.14797,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "37712236+popescuoctavian@users.noreply.github.com",
+            "name": "Octavian Popescu",
+            "username": "popescuoctavian"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17be322b56637a263182e4f1f8895539a9f94e3b",
+          "message": "chore: change error description when encountering an invalid Error(string) (#1051)",
+          "timestamp": "2025-08-08T19:59:56Z",
+          "tree_id": "abfde423ef2ff43f202cab4ee43eafc2222d4aa6",
+          "url": "https://github.com/NomicFoundation/edr/commit/17be322b56637a263182e4f1f8895539a9f94e3b"
+        },
+        "date": 1754685831092,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 211618.65178299998,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 21504.960013,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 10651.342252,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 12426.981230000001,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 756.672573,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_585b2ef8",
+            "value": 5527.891431,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 156462.359748,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 4288.444536,
             "unit": "ms"
           }
         ]
