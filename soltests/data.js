db47ad30f47d3628f339965867cdf20c14260c66
Agost Biro <agostbiro@gmail.com>
2025-11-17 10:46:38 +0000

    Add back results for 00d3901

--- soltests/data.js
+++ soltests/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763145948838,
+  "lastUpdate": 1763375731202,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
@@ -11177,6 +11177,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "StdUtilsForkTest",
             "value": 151000,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "5764438+agostbiro@users.noreply.github.com",
+            "name": "Agost Biro",
+            "username": "agostbiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "fc7271dd4e89a08235e55b16660232d749dc2e6c",
+          "message": "ci: migrate soltest benchmarks to microseconds (#1193)",
+          "timestamp": "2025-11-17T09:21:28Z",
+          "tree_id": "4f5cf7f68d64c6e37dcd81b75f82b62a8d7573a9",
+          "url": "https://github.com/NomicFoundation/edr/commit/fc7271dd4e89a08235e55b16660232d749dc2e6c"
+        },
+        "date": 1763375730721,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total",
+            "value": 33023874,
+            "unit": "us"
+          },
+          {
+            "name": "StdCheatsForkTest",
+            "value": 10670,
+            "unit": "us"
+          },
+          {
+            "name": "StdCheatsTest",
+            "value": 280860,
+            "unit": "us"
+          },
+          {
+            "name": "StdMathTest",
+            "value": 8795,
+            "unit": "us"
+          },
+          {
+            "name": "StdStorageTest",
+            "value": 32847446,
+            "unit": "us"
+          },
+          {
+            "name": "StdUtilsForkTest",
+            "value": 149681,
             "unit": "us"
           }
         ]
