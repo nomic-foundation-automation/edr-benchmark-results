c9499eb6fab3bfc4966af103fe40b7760ebe2675
github-action-benchmark <github@users.noreply.github.com>
2025-12-18 22:40:19 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for bbc09f31d507cd9ff204b6eb7f04588747827980

--- bench/data.js
+++ bench/data.js
@@ -1,72 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765903626171,
+  "lastUpdate": 1766097619579,
   "repoUrl": "https://github.com/NomicFoundation/edr",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "Wodann@users.noreply.github.com",
-            "name": "Wodann",
-            "username": "Wodann"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "8c1f798653b93da69a53758148316b67f2d8d1ff",
-          "message": "fix: always return `None` for empty accounts in `RemoteState` (#1125)",
-          "timestamp": "2025-10-21T20:55:13Z",
-          "tree_id": "0cbf31fe91746ce05e75879a19d723e45a8b564b",
-          "url": "https://github.com/NomicFoundation/edr/commit/8c1f798653b93da69a53758148316b67f2d8d1ff"
-        },
-        "date": 1761082544977,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "All Scenarios",
-            "value": 222970.74732399997,
-            "unit": "ms"
-          },
-          {
-            "name": "neptune-mutual-blue-protocol_8db6480",
-            "value": 21818.757156,
-            "unit": "ms"
-          },
-          {
-            "name": "openzeppelin-contracts_0a5fba7a",
-            "value": 11229.544657,
-            "unit": "ms"
-          },
-          {
-            "name": "rocketpool_6a9dbfd8",
-            "value": 12609.760589,
-            "unit": "ms"
-          },
-          {
-            "name": "safe-contracts_914d0f8",
-            "value": 739.8304430000001,
-            "unit": "ms"
-          },
-          {
-            "name": "seaport_585b2ef8",
-            "value": 5856.218098,
-            "unit": "ms"
-          },
-          {
-            "name": "synthetix_9a3a109f",
-            "value": 166188.10622,
-            "unit": "ms"
-          },
-          {
-            "name": "uniswap-v3-core_d8b1c63",
-            "value": 4528.530161,
-            "unit": "ms"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -3199,6 +3135,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "uniswap-v3-core_d8b1c63",
             "value": 4602.0904900000005,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aghiglia@manas.tech",
+            "name": "Ana Perez Ghiglia",
+            "username": "anaPerezGhiglia"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bbc09f31d507cd9ff204b6eb7f04588747827980",
+          "message": "chore(CI): configure dependabot cooldown (#1233)",
+          "timestamp": "2025-12-18T20:50:20Z",
+          "tree_id": "68c2b2f7557ea09fb5bc0f891261dfe1c3272e5b",
+          "url": "https://github.com/NomicFoundation/edr/commit/bbc09f31d507cd9ff204b6eb7f04588747827980"
+        },
+        "date": 1766097619127,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "All Scenarios",
+            "value": 212618.179084,
+            "unit": "ms"
+          },
+          {
+            "name": "neptune-mutual-blue-protocol_8db6480",
+            "value": 18986.78499,
+            "unit": "ms"
+          },
+          {
+            "name": "openzeppelin-contracts_0a5fba7a",
+            "value": 9952.763111,
+            "unit": "ms"
+          },
+          {
+            "name": "rocketpool_6a9dbfd8",
+            "value": 11552.206355,
+            "unit": "ms"
+          },
+          {
+            "name": "safe-contracts_914d0f8",
+            "value": 648.4782479999999,
+            "unit": "ms"
+          },
+          {
+            "name": "seaport_585b2ef8",
+            "value": 5369.998465,
+            "unit": "ms"
+          },
+          {
+            "name": "synthetix_9a3a109f",
+            "value": 161955.184059,
+            "unit": "ms"
+          },
+          {
+            "name": "uniswap-v3-core_d8b1c63",
+            "value": 4152.763856,
             "unit": "ms"
           }
         ]
